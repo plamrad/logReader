12ce9c9ed59c6b9394e1856a75c2afeeabd11d14
plamrad <plamrad@gmail.com>
2017-07-17 14:25:48 +0300

    Create createLogFile.php

---
+++ Helper/createLogFile.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @author Plam Rad <plamrad@gmail.com>
+ */
+require_once 'Classes/LogDataCreator.php';
+
+$filename = 'LogFiles/logs';
+$logDataCreator = new LogDataCreator($filename, '2017-07-12 09:00:00', '2017-07-12 10:01:00');
+echo $logDataCreator->createDummyData();
