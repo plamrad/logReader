ab3e9bdff43fc4eb21e7b4141bd0c611231ec946
Radu 5Log <radu5log@radu5log.com>
2017-08-01 16:28:55 +0300

    relative path to classes

--- Helper/createLogFile.php
+++ Helper/createLogFile.php
@@ -3,8 +3,8 @@
 /**
  * @author Plam Rad <plamrad@gmail.com>
  */
-require_once 'Classes/LogDataCreator.php';
+require_once '../Classes/LogDataCreator.php';
 
-$filename = 'LogFiles/logs';
+$filename = '../LogFiles/logs';
 $logDataCreator = new LogDataCreator($filename, '2017-07-12 09:00:00', '2017-07-12 10:01:00');
 echo $logDataCreator->createDummyData();
